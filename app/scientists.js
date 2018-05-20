7c0ce226292535d5b51f4e287952dced7be45ff8
UKyz <ukyz@icloud.com>
2018-05-20 11:10:56 +0200

    Update scientists and scientists-test, check xo

--- app/scientists.js
+++ app/scientists.js
@@ -5,53 +5,80 @@ class Scientists {
     this.philosopherLvl_ = 0;
     this.economistLvl_ = 0;
     this.architectLvl_ = 0;
+    this.mathematicianGlobalLvl_ = 0;
+    this.physicianGlobalLvl_ = 0;
+    this.philosopherGlobalLvl_ = 0;
+    this.economistGlobalLvl_ = 0;
+    this.architectGlobalLvl_ = 0;
 
     this.costCorn_ = 1;
     this.costWood_ = 1;
     this.lessWoodRequired_ = 0;
+    this.unitsDamage_ = 0;
     this.timeFactor_ = timeFactors;
   }
 
-  mathematicianLvlUp() {
-    if (this.mathematicianLvl_ < 5) {
-      this.mathematicianLvl_++;
+  mathematicianLvlUp(test) {
+    if (this.mathematicianGlobalLvl_ < 5) {
+      this.mathematicianGlobalLvl_++;
+      if (test === true) {
+        this.mathematicianLvl_++;
+      }
     }
   }
 
-  physicianLvlUp() {
-    if (this.physicianLvl_ < 5) {
-      this.physicianLvl_++;
+  physicianLvlUp(test) {
+    if (this.physicianGlobalLvl_ < 5) {
+      this.physicianGlobalLvl_++;
+      if (test === true) {
+        this.physicianLvl_++;
+        return new Promise(resolve => {
+          setTimeout(() => {
+            this.unitsDamage_ += 3;
+            resolve();
+          }, 40 * this.timeFactor_);
+        });
+      }
     }
   }
 
-  philosopherLvlUp() {
-    if (this.philosopherLvl_ < 5) {
-      this.philosopherLvl_++;
+  philosopherLvlUp(test) {
+    if (this.philosopherGlobalLvl_ < 5) {
+      this.philosopherGlobalLvl_++;
+      if (test === true) {
+        this.philosopherLvl_++;
+      }
     }
   }
 
-  economistLvlUp() {
-    if (this.economistLvl_ < 5) {
-      this.economistLvl_++;
-      return new Promise(resolve => {
-        setTimeout(() => {
-          this.costCorn_++;
-          this.costWood_++;
-          resolve();
-        }, 50 * this.timeFactor_);
-      });
+  economistLvlUp(test) {
+    if (this.economistGlobalLvl_ < 5) {
+      this.economistGlobalLvl_++;
+      if (test === true) {
+        this.economistLvl_++;
+        return new Promise(resolve => {
+          setTimeout(() => {
+            this.costCorn_++;
+            this.costWood_++;
+            resolve();
+          }, 50 * this.timeFactor_);
+        });
+      }
     }
   }
 
-  architectLvlUp() {
-    if (this.architectLvl_ < 5) {
-      this.architectLvl_++;
-      return new Promise(resolve => {
-        setTimeout(() => {
-          this.lessWoodRequired_ += 2;
-          resolve();
-        }, 40 * this.timeFactor_);
-      });
+  architectLvlUp(test) {
+    if (this.architectGlobalLvl_ < 5) {
+      this.architectGlobalLvl_++;
+      if (test === true) {
+        this.architectLvl_++;
+        return new Promise(resolve => {
+          setTimeout(() => {
+            this.lessWoodRequired_ += 2;
+            resolve();
+          }, 40 * this.timeFactor_);
+        });
+      }
     }
   }
 
@@ -59,20 +86,40 @@ class Scientists {
     return this.mathematicianLvl_;
   }
 
+  get mathematicianGlobalLvl() {
+    return this.mathematicianGlobalLvl_;
+  }
+
   get physicianLvl() {
     return this.physicianLvl_;
+  }
+
+  get physicianGlobalLvl() {
+    return this.physicianGlobalLvl_;
   }
 
   get philosopherLvl() {
     return this.philosopherLvl_;
   }
 
+  get philosopherGlobalLvl() {
+    return this.philosopherGlobalLvl_;
+  }
+
   get economistLvl() {
     return this.economistLvl_;
   }
 
+  get economistGlobalLvl() {
+    return this.economistGlobalLvl_;
+  }
+
   get architectLvl() {
     return this.architectLvl_;
+  }
+
+  get architectGlobalLvl() {
+    return this.architectGlobalLvl_;
   }
 
   get timeFactor() {
--- test/scientists-test.js
+++ test/scientists-test.js
@@ -26,11 +26,11 @@ describe('scientists.js', () => {
       g.economistLvl.should.be.equal(0);
       g.architectLvl.should.be.equal(0);
 
-      g.mathematicianLvlUp();
-      g.physicianLvlUp();
-      g.philosopherLvlUp();
-      g.economistLvlUp();
-      g.architectLvlUp();
+      g.mathematicianLvlUp(true);
+      g.physicianLvlUp(true);
+      g.philosopherLvlUp(true);
+      g.economistLvlUp(true);
+      g.architectLvlUp(true);
 
       g.mathematicianLvl.should.be.equal(1);
       g.physicianLvl.should.be.equal(1);
@@ -38,13 +38,31 @@ describe('scientists.js', () => {
       g.economistLvl.should.be.equal(1);
       g.architectLvl.should.be.equal(1);
 
-      g.economistLvlUp();
-      g.economistLvlUp();
-      g.economistLvlUp();
-      g.economistLvlUp();
-      g.economistLvlUp();
+      g.mathematicianLvlUp(false);
+      g.physicianLvlUp(false);
+      g.philosopherLvlUp(false);
+      g.economistLvlUp(false);
+      g.architectLvlUp(false);
 
-      g.economistLvl.should.be.equal(5);
+      g.mathematicianLvl.should.be.equal(1);
+      g.physicianLvl.should.be.equal(1);
+      g.philosopherLvl.should.be.equal(1);
+      g.economistLvl.should.be.equal(1);
+      g.architectLvl.should.be.equal(1);
+      g.mathematicianGlobalLvl.should.be.equal(2);
+      g.physicianGlobalLvl.should.be.equal(2);
+      g.philosopherGlobalLvl.should.be.equal(2);
+      g.economistGlobalLvl.should.be.equal(2);
+      g.architectGlobalLvl.should.be.equal(2);
+
+      g.economistLvlUp(true);
+      g.economistLvlUp(true);
+      g.economistLvlUp(true);
+      g.economistLvlUp(true);
+      g.economistLvlUp(true);
+
+      g.economistLvl.should.be.equal(4);
+      g.economistGlobalLvl.should.be.equal(5);
     });
   });
 
@@ -61,20 +79,23 @@ describe('scientists.js', () => {
     it('should change the resources features', async () => {
       g.economistLvl.should.be.equal(0);
       g.architectLvl.should.be.equal(0);
+      g.physicianLvl.should.be.equal(0);
 
-      await g.economistLvlUp();
-      await g.architectLvlUp();
+      await g.economistLvlUp(true);
+      await g.architectLvlUp(true);
+      await g.physicianLvlUp(true);
 
       g.costCorn.should.be.equal(2);
       g.costWood.should.be.equal(2);
       g.lessWoodRequired.should.be.equal(2);
+      g.unitsDamage_.should.be.equal(3);
 
-      await g.architectLvlUp();
+      await g.architectLvlUp(true);
       g.lessWoodRequired.should.be.equal(4);
-      await g.architectLvlUp();
-      await g.architectLvlUp();
-      await g.architectLvlUp();
-      await g.architectLvlUp();
+      await g.architectLvlUp(true);
+      await g.architectLvlUp(true);
+      await g.architectLvlUp(true);
+      await g.architectLvlUp(true);
       g.lessWoodRequired.should.be.equal(10);
     });
   });
